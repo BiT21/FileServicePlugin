894d9bfc13553fa3f14a1027317bf87829bbdf6f
raulg <raul.gonzalez-rodriguez@sogeti.com>
2018-02-20 19:51:45 +0100

    FileService.Core.Test

---
+++ src.NetStandard/FileService.Core.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileService.NetStandard20.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
